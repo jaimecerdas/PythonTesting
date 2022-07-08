94c3323bf2d8edd77fdabf69e0eb6ddb27db34f4
J Cerdas <jaimecerdas@gmail.com>
2022-07-08 04:50:21 +0000

    WhatsApp Loop

---
+++ WhatsAppv1.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+loop = 2
+
+while loop > 0:
+    pywhatkit.sendwhatmsg_instantly('+506########', ":)", 5, True, 6)
+    loop -= 1
+
+#Loop for https://github.com/Ankit404butfound/PyWhatKit/wiki/Sending-WhatsApp-Messages
